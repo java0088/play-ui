ef294ee200825e97d2f76e46d8c371c058c4f4a2
huangwantong <huangwantong@escopetech.com>
2022-08-10 13:03:11 +0800

    style: eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,23 +1,34 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   extends: [
-    'prettier',
+    'eslint:recommended',
+    'plugin:vue/recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
     'plugin:prettier/recommended',
-    'plugin:vue/recommended'
   ],
   env: {
     browser: true,
     es2021: true
   },
   extends: ['eslint:recommended'],
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'vue'],
   parserOptions: {
-    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
     'prettier/prettier': 'error',
-    'no-extra-semi': 'off'
+    'no-extra-semi': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-extra-semi': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off'
   }
 }
--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -3,7 +3,7 @@ export const withInstall = <T, E extends Record<string, any>>(
   main: T,
   extra?: E
 ) => {
-  ; (main as any).install = (app: App): void => {
+  ;(main as any).install = (app: App): void => {
     for (const comp of [main, ...Object.values(extra ?? {})]) {
       app.component(comp.name, comp)
     }
@@ -11,7 +11,7 @@ export const withInstall = <T, E extends Record<string, any>>(
 
   if (extra) {
     for (const [key, comp] of Object.entries(extra)) {
-      ; (main as any)[key] = comp
+      ;(main as any)[key] = comp
     }
   }
   return main
